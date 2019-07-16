4bbdf00058f195569af3acd65e2762547038b9d0
PhilipStubbs <24510543+PhilipStubbs@users.noreply.github.com>
2019-07-16 13:59:54 +0200

    got WTC students

--- Swifty Companion/Swifty Companion/FirstViewController.swift
+++ Swifty Companion/Swifty Companion/FirstViewController.swift
@@ -8,38 +8,70 @@
 
 import UIKit
 
-class FirstViewController: UIViewController, UITableViewDataSource {
+class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
  
     var studentArray = [Student]()
-
+    var currentStudentArray = [Student]()
+    let file = "students.txt"
+    
     @IBOutlet var table: UITableView!
     @IBOutlet var searchBar: UISearchBar!
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpStudents()
+        setUpSearchBar()
     }
     
     private func setUpStudents(){
-        studentArray.append(Student(name:"Philip", campus:"WeThinkCode_", image:"default"))
-        studentArray.append(Student(name:"Kirsten", campus:"WeThinkCode_", image:"default"))
-        studentArray.append(Student(name:"Xeno", campus:"WeThinkCode_", image:"default"))
-        studentArray.append(Student(name:"Teo", campus:"WeThinkCode_", image:"default"))
-        studentArray.append(Student(name:"Talon", campus:"WeThinkCode_", image:"default"))
-        studentArray.append(Student(name:"Sam", campus:"WeThinkCode_", image:"default"))
+        if let asset = NSDataAsset(name: "Data") {
+            let data = String(data: asset.data, encoding: .utf8)
+            let myStrings = (data?.components(separatedBy: .newlines))!
+            
+            for name in myStrings {
+                studentArray.append(Student(name:name.lowercased(), campus:"WeThinkCode_", image:"default"))
+            }
+        } else {
+            print("Students Not Found")
+        }
+        
+        currentStudentArray = studentArray
     }
+    
+    private func setUpSearchBar(){
+        searchBar.delegate = self
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return studentArray.count
+        return currentStudentArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as? TableCell else {
             return UITableViewCell()
         }
-        cell.name.text = studentArray[indexPath.row].name
-        cell.campus.text = studentArray[indexPath.row].campus
-        cell.intraProfilePicture.image = UIImage(named: studentArray[indexPath.row].image)
+        cell.name.text = currentStudentArray[indexPath.row].name
+        cell.campus.text = currentStudentArray[indexPath.row].campus
+        cell.intraProfilePicture.image = UIImage(named: currentStudentArray[indexPath.row].image)
         
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String){
+        guard !searchText.isEmpty else {
+            currentStudentArray = studentArray
+            table.reloadData()
+            return
+        }
+        
+        currentStudentArray = studentArray.filter({ student -> Bool in
+            guard let text = searchBar.text else {return false}
+            return student.name.contains(text.lowercased())
+        })
+        table.reloadData()
     }
 
 }
