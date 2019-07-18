46907f2e0cf097b968a80358de53088e915cf170
PhilipStubbs <24510543+PhilipStubbs@users.noreply.github.com>
2019-07-18 10:59:46 +0200

    post and get working

--- Swifty Companion/Swifty Companion/SecondViewController.swift
+++ Swifty Companion/Swifty Companion/SecondViewController.swift
@@ -11,7 +11,12 @@ import UIKit
 
 class SecondViewController: UIViewController {
     
+   static let authFileName = "Swifty-Companion-Auth"
+   static let DocumentDirURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+   static let fileURL = DocumentDirURL.appendingPathComponent(authFileName).appendingPathExtension("txt")
+    
     var detailedStudent: DetailedStudent = DetailedStudent();
+    var authKey: String = ""
     
     @IBOutlet var profilePicture: UIImageView!
     @IBOutlet var userName: UILabel!
@@ -26,19 +31,33 @@ class SecondViewController: UIViewController {
         userEmail.text = detailedStudent.email
         campus.text = detailedStudent.campus
         profilePicture.image = detailedStudent.profilePicture
+
+        findAuthKey()
         getUserInfo()
     }
+    
+
     
     func getUserInfo(){
         var jsonResponse = ""
         
         let url = URL(string: "https://api.intra.42.fr/v2/users/"+detailedStudent.name+"/cursus_users")!
-        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+        let token = authKey
+        var request = URLRequest(url:url)
+        request.addValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        
+       
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
             if let error = error {
                 print("error: \(error)")
             } else {
                 if let response = response as? HTTPURLResponse {
                     print("statusCode: \(response.statusCode)")
+                    if (response.statusCode == 401){
+                        self.getNewAuthKey()
+                        self.findAuthKey()
+                        self.getUserInfo()
+                    }
                 }
                 if let data = data, let dataString = String(data: data, encoding: .utf8) {
                     jsonResponse = dataString
@@ -51,20 +70,84 @@ class SecondViewController: UIViewController {
         
     }
     
-    func getAuthKey(){
+ 
+    
+    func getNewAuthKey() {
+        var jsonResponse = ""
+//
+        let url = URL(string: "https://api.intra.42.fr/oauth/token")!
+////
+        let parameters = [
+            "grant_type":"client_credentials",
+            "client_id": "2395c8f300a32a81e07519353dee66e3e719ba6aa3d176541e6fe1408a4fa83d" ,
+            "client_secret":"5c2fd78404ed862bd2a59d9ad6c2097bd4114029e0615556bcc49432aad13a61"
+        ]
         
+        //create the session object
+        let session = URLSession.shared
+        
+        //now create the URLRequest object using the url object
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST" //set http method as POST
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted) // pass dictionary to nsdata object and set it as request body
+        } catch let error {
+            print(error.localizedDescription)
+        }
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        
+        //create dataTask using the session object to send data to the server
+        let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
+            
+            guard error == nil else {
+                return
+            }
+            if let response = response as? HTTPURLResponse {
+                print("statusCode: \(response.statusCode)")
+            }
+            guard let data = data else {
+                return
+            }
+            
+            do {
+                //create json object from data
+                if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
+                    self.writeAuthKey(text: json["access_token"] as! String)
+                    // handle json...
+                }
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        })
+        task.resume()
     }
+    
+    // Writing Auth key to File, to be used later
+    func writeAuthKey(text: String){
+        
+        do {
+            try text.write(to: SecondViewController.fileURL, atomically: true, encoding: .utf8)
+            print("writing to :" + SecondViewController.authFileName + text )
+        } catch {
+            print("failed with error: \(error)")
+        }
 
-//    func setProfilePicture() {
-//        let url = URL(string: "https://cdn.intra.42.fr/users/medium_"+detailedStudent.name+".png")!
-//        do {
-//            let data = try Data(contentsOf: url)
-//            profilePicture.image = UIImage(data: data)
-//        } catch {
-//            profilePicture.image = UIImage(named: "default")
-//        }
-//    }
-
+    }
+    
+    // Finding the Auth key file
+    func findAuthKey(){
+        do {
+            let text = try String(contentsOf: SecondViewController.fileURL, encoding: .utf8)
+            authKey = text
+            print("Read back text: \(authKey)")
+        }
+        catch {
+            print("failed with error: \(error)")
+        }
+    }
     
     @IBAction func backButton(_ sender: Any) {
         performSegue(withIdentifier: "displayAllUsers", sender: self)
